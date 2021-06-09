f2dd70ef1e62ef12a1abca61e0e8f82d234bf0f5
KAZUO TSUTSUI <kzt206@gmail.com>
2021-06-09 21:56:55 +0900

    Push Image first success

--- Oekaki06Basic/canvas.js
+++ Oekaki06Basic/canvas.js
@@ -12,9 +12,44 @@ window.addEventListener("load",()=>{
    canvas02.height = 300;
    
     ctx01.fillRect(10,10,30,20);
-    ctx02.fillRect(20,30,50,100);
+    // ctx02.fillRect(20,30,50,100);
 
+    console.log("hello");
 
+    const buttonTest = document.getElementById("testButton");
+    buttonTest.addEventListener("click",function(event){
+        console.log("button pushed");
+    });
+
+    const selectFile01 = document.getElementById("selectFile01");
+    selectFile01.addEventListener("change",function(evt){
+
+        console.log("file selector");
+
+        let file = evt.target.files;
+        let reader = new FileReader();
+
+        reader.readAsDataURL(file[0]);
+
+        console.log(file[0]);
+
+        reader.onload = function(){
+            let dataURL = reader.result;
+            let img = new Image();
+
+            img.src = dataURL;
+
+            img.onload = function(){
+                ctx01.drawImage(img,0,0,canvas01.width,canvas01.height);
+            }
+            ctx02.fillRect(50,90,30,20);
+        }
+
+    },false);
+
+    console.log("hello2");
+    // ctx01.drawImage(img,0,0,canvas01.width,canvas01.heigth);
+    ctx01.fillRect(30,50,30,20);
 
 
 });
