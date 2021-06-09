97353e3d616ef3c51cc1550bf2243515adcf5a03
KAZUO TSUTSUI <kzt206@gmail.com>
2021-06-09 21:20:35 +0900

    Change var -> const

--- Oekaki06Basic/canvas.js
+++ Oekaki06Basic/canvas.js
@@ -1,9 +1,9 @@
 window.addEventListener("load",()=>{
 
-    var canvas01 = document.querySelector("#canvas01");
-    var ctx01 = canvas01.getContext("2d");
-    var canvas02 = document.querySelector("#canvas02");
-    var ctx02 = canvas02.getContext("2d");
+    const canvas01 = document.querySelector("#canvas01");
+    const ctx01 = canvas01.getContext("2d");
+    const canvas02 = document.querySelector("#canvas02");
+    const ctx02 = canvas02.getContext("2d");
 
    canvas01.width = 300;
    canvas01.height = 200;
@@ -11,7 +11,10 @@ window.addEventListener("load",()=>{
    canvas02.width = 500;
    canvas02.height = 300;
    
-    ctx01.fillRect(10,10,20,20);
+    ctx01.fillRect(10,10,30,20);
     ctx02.fillRect(20,30,50,100);
+
+
+
 
 });
