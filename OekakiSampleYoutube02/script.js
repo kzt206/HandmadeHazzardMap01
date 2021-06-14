052c6bbc6a4259992ee6034ae23a4ca6179d752b
kzt206 <kzt206@gmail.com>
2021-06-14 11:43:57 +0900

    Build Complete

--- OekakiSampleYoutube02/script.js
+++ OekakiSampleYoutube02/script.js
@@ -27,6 +27,7 @@ canvas.addEventListener("mousemove",(event)=>{
 });
 
 ctx.fillStyle = "hotpink";
+ctx.strokeStyle = ctx.fillStyle;
 
 function draw(x2,y2){
     if(isDrawing){
@@ -34,8 +35,47 @@ function draw(x2,y2){
         ctx.arc(x2,y2,penSize,0,Math.PI * 2);
         ctx.closePath();
         ctx.fill();
+        //draw line
+        drawLine(x,y,x2,y2);
     }
-
-
-
+    x = x2;
+    y = y2;
 }
+
+function drawLine(x1,y1,x2,y2){
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.strokeStyle = ctx.fillStyle;
+    ctx.lineWidth = penSize * 2;
+    ctx.stroke();
+}
+
+document.querySelector(".fa-refresh").addEventListener("click",function(){
+    ctx.clearRect(0, 0,canvas.width,canvas.height);
+});
+
+const selectColor = (elem) => {
+
+    removeActiveCircleColor();
+
+    ctx.fillStyle = elem.getAttribute("data-color");
+    elem.classList.add("active");
+};
+
+const removeActiveCircleColor = () =>{
+    colorCircle.forEach((circle)=>{
+        circle.classList.remove("active");
+    });
+};
+
+function penSizeChange(pensize){
+    penSize = pensize
+}
+
+const favColor = (elem) => {
+    removeActiveCircleColor();
+    ctx.fillStyle = elem.value;
+};
+
+document.querySelector("a").addEventListener("click",(event)=> event.target.href = canvas.toDataURL());
