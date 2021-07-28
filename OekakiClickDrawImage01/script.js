581736dc34f69955f75d5f35851ae996436272f4
kzt206 <kzt206@gmail.com>
2021-07-28 15:52:39 +0900

    Add stamp images

--- OekakiClickDrawImage01/script.js
+++ OekakiClickDrawImage01/script.js
@@ -33,18 +33,40 @@ window.onload = () =>{
   const ctx = board.getContext("2d");
 
   // 画像読み込み
-  const chara = new Image();
-  chara.src = "pin2.png";  // 画像のURLを指定
-  chara.onload = () => {
-    ctx.drawImage(chara, 100, 150);
-  };
+  let chara = new Image();
+  const chara1 = new Image();
+  chara1.src = "school01.png";  // 画像のURLを指定
+  const chara2 = new Image();
+  chara2.src = "pin2.png"
+//   chara.onload = () => {
+//     ctx.drawImage(chara, 100, 150);
+//   };
 
   this.canvas1.addEventListener("click",(e) => {
       let x = e.offsetX;
       let y = e.offsetY;
 
       console.log("x:",x,"y:",y);
-      ctx.drawImage(chara,x,y);
+      console.log("Penstatus;",penStatus);
+      if(penStatus == 1) {
+        ctx.drawImage(chara1,x,y);
+      } else if(penStatus == 2) {
+        ctx.drawImage(chara2,x,y);
+      }
+      
+  })
+
+  const stamp01 = document.getElementById("stamp01");
+  const stamp02 = document.getElementById("stamp02");
+  let penStatus=0;
+
+  stamp01.addEventListener("click",()=>{
+      console.log("stamp01 is pushed");
+      penStatus = 1;
+    })
+  stamp02.addEventListener("click",()=>{
+        console.log("stamp02 is pushed");
+        penStatus = 2;
   })
 
 };
